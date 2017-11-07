ae6badd997b44c9441c19f2c7a129a4df66c2835
yankfy <yankfy@qq.com>
2017-11-07 23:46:00 +0800

    顶部组件弹出层样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,18 +17,28 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detial-wrapper clearfix">
+        <div class="detail-main">
+
+        </div>
+      </div>
+      <div class="detail-close" @click="hideDetail">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -41,10 +51,20 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      detailShow: false
+    }
   },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true
+    },
+    hideDetail() {
+      this.detailShow = false
+    }
   }
 }
 </script>
@@ -183,6 +203,32 @@ export default {
     height: 100%;
     z-index: -1;
     filter: blur(10px);
+  }
+  .detail {
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(7, 17, 27, 0.8);
+    .detial-wrapper {
+      min-height: 100%;
+      .detail-main {
+        margin-top: 64px;
+        // padding必须
+        padding-bottom: 64px;
+      }
+    }
+    .detail-close {
+      position: relative;
+      width: 32px;
+      height: 32px;
+      margin: -64px auto 0 auto;
+      clear: both;
+      font-size: 32px;
+    }
   }
 }
 </style>
