7f53b38488c485baf3d8e0221e34a8b387c3ae36
yankfy <yankfy@qq.com>
2017-11-08 22:57:58 +0800

    顶部组件over

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,20 +30,45 @@
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
     </div>
-    <div class="detail" v-show="detailShow">
-      <div class="detial-wrapper clearfix">
-        <div class="detail-main">
-
+    <transition name="fade">
+      <div class="detail" v-show="detailShow">
+        <div class="detial-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports" :key="index">
+                <span class="icon" :class="classMap[item.type]"></span>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detail-close" @click="hideDetail">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import star from '../../components/star/star'
 export default {
   props: {
     seller: {
@@ -65,170 +90,14 @@ export default {
     hideDetail() {
       this.detailShow = false
     }
+  },
+  components: {
+    star
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../common/scss/mixin';
-.header {
-  position: relative;
-  color: #ffffff;
-  background-color: rgba(7, 17, 27, 0.5);
-  overflow: hidden;
-  .content-wrapper {
-    position: relative;
-    padding: 24px 12px 18px 24px;
-    font-size: 0;
-    .avatar {
-      display: inline-block;
-      margin-right: 16px;
-      vertical-align: top;
-      img {
-        border-radius: 2px;
-      }
-    }
-    .content {
-      display: inline-block;
-      .title {
-        margin: 2px 0 8px 0;
-        .brand {
-          display: inline-block;
-          vertical-align: top;
-          width: 30px;
-          height: 18px;
-          @include bg-image('brand');
-          background-size: 30px 18px;
-          background-repeat: no-repeat;
-        }
-        .name {
-          margin-left: 6px;
-          font-size: 16px;
-          line-height: 18px;
-          font-weight: bold;
-        }
-      }
-      .description {
-        margin-bottom: 10px;
-        line-height: 12px;
-        font-size: 12px;
-      }
-      .support {
-        .icon {
-          display: inline-block;
-          width: 12px;
-          height: 12px;
-          margin-right: 4px;
-          background-size: 12px 12px;
-          background-repeat: no-repeat;
-          vertical-align: top;
-          &.decrease {
-            @include bg-image('decrease_1');
-          }
-          &.discount {
-            @include bg-image('discount_1');
-          }
-          &.guarantee {
-            @include bg-image('guarantee_1');
-          }
-          &.invoice {
-            @include bg-image('invoice_1');
-          }
-          &.special {
-            @include bg-image('special_1');
-          }
-        }
-        .text {
-          line-height: 12px;
-          font-size: 10px;
-        }
-      }
-    }
-    .support-count {
-      position: absolute;
-      right: 12px;
-      bottom: 14px;
-      padding: 0 8px;
-      height: 24px;
-      line-height: 24px;
-      border-radius: 14px;
-      background-color: rgba(#000000, 0.2);
-      text-align: center;
-      .count {
-        font-size: 10px;
-      }
-      .icon-keyboard_arrow_right {
-        margin-left: 2px;
-        line-height: 24px;
-        font-size: 10px;
-      }
-    }
-  }
-  .bulletin-wrapper {
-    position: relative;
-    height: 28px;
-    line-height: 28px;
-    padding: 0 22px 0 12px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    background-color: rgba(7, 17, 27, 0.2);
-    .bulletin-title {
-      display: inline-block;
-      width: 22px;
-      height: 12px;
-      vertical-align: top;
-      margin-top: 8px;
-      @include bg-image('bulletin');
-      background-size: 22px 12px;
-      background-repeat: no-repeat;
-    }
-    .bulletin-text {
-      margin: 0 4px;
-      font-size: 10px;
-      vertical-align: top;
-    }
-    .icon-keyboard_arrow_right {
-      position: absolute;
-      top: 8px;
-      right: 12px;
-      font-size: 10px;
-    }
-  }
-  .background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: -1;
-    filter: blur(10px);
-  }
-  .detail {
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    background-color: rgba(7, 17, 27, 0.8);
-    .detial-wrapper {
-      min-height: 100%;
-      .detail-main {
-        margin-top: 64px;
-        // padding必须
-        padding-bottom: 64px;
-      }
-    }
-    .detail-close {
-      position: relative;
-      width: 32px;
-      height: 32px;
-      margin: -64px auto 0 auto;
-      clear: both;
-      font-size: 32px;
-    }
-  }
-}
+@import './header';
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="star" :class="starType">
+        <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item" :key="itemClass"></span>
+    </div>
+</template>
+
+<script>
+const LENGHT = 5
+const CLS_ON = 'on'
+const CLS_HALF = 'half'
+const CLS_OFF = 'off'
+export default {
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    starType() {
+      return 'star-' + this.size
+    },
+    itemClasses() {
+      let result = []
+      let score = Math.floor(this.score * 2) / 2
+      let hasDecimal = score % 1 !== 0
+      let integer = Math.floor(score)
+      for (let i = 0; i < integer; i++) {
+        result.push(CLS_ON)
+      }
+      if (hasDecimal) result.push(CLS_HALF)
+      while (result.length < LENGHT) {
+        result.push(CLS_OFF)
+      }
+      return result
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '../common/scss/mixin';
+@import './star';
+</style>
