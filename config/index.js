283aa2b4181aa663943a0a18cab5369ec8e21118
yankfy <yankfy@qq.com>
2017-11-14 21:35:37 +0800

    修改商品组件

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 8080,
+    port: process.env.PORT || 800,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-        商品
+    <div class="goods">
+        <div class="menu-wrapper"></div>
+        <div class="foods-wrapper"></div>
     </div>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .goods {
+    display: flex;
+    position: absolute;
+    top: 174px;
+    bottom: 46px;
+    width: 100%;
+    overflow: hidden;
+    .menu-wrapper {
+      width: 80px;
+      flex-basis: 80px;
+      background-color: #f3f5f7;
+    }
+    .foods-wrapper {
+      flex: 1;
+      background-color:#fff; 
+    }
+  }
 </style>
