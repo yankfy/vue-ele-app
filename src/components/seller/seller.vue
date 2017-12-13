0da01614681fa8ee1285a96957a6c5aae1e1cc64
yankfy <yankfy@qq.com>
2017-12-13 22:59:28 +0800

    seller overview HTML

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,11 +1,58 @@
 <template>
-    <div>
-        商家
+    <div class="seller" ref="seller">
+        <div class="seller-content">
+            <div class="overview">
+                <h1 class="title">{{seller.name}}</h1>
+                <div class="desc">
+                    <star :size="36" :score="seller.score"></star>
+                    <span class="text">{{seller.ratingCount}}</span>
+                    <span class="text">{{seller.sellCount}}</span>
+                </div>
+                <ul class="remark">
+                    <li class="block">
+                        <h2 class="title">起送价</h2>
+                        <div class="content">
+                            <span class="stress">{{seller.minPrice}}</span>元
+                        </div>
+                    </li>
+                    <li class="block">
+                        <h2 class="title">商家配送</h2>
+                        <div class="content">
+                            <span class="stress">{{seller.deliveryPrice}}</span>元
+                        </div>
+                    </li>
+                    <li class="block">
+                        <h2 class="title">平均配送时间</h2>
+                        <div class="content">
+                            <span class="stress">{{seller.deliveryTime}}</span>分钟
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+import star from '../star/star'
+import BScorll from 'better-scroll'
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.scroll = new BScorll(this.$refs.seller, {
+        click: true
+      })
+    })
+  },
+  components: {
+    star
+  }
+}
 </script>
 
 <style>
