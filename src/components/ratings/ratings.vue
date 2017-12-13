4228c3c03e6b128dc2d4324fc134c82399bfef06
yankfy <yankfy@qq.com>
2017-12-13 22:24:31 +0800

    评价页面组件完成

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,63 +1,68 @@
 <template>
-    <div>
-        <div class="ratings">
-            <div class="ratings-content">
-                <div class="overview">
-                    <div class="overview-left">
-                        <h1 class="score">{{seller.score}}</h1>
-                        <div class="title">综合评分</div>
-                        <div class="rank">高于周边商家{{seller.rankRate}}%</div>
-                    </div>
-                    <div class="overview-right">
-                        <div class="score-wrapper">
-                            <span class="title">服务态度</span>
-                            <star :size='36' :score='seller.serviceScore'></star>
-                            <span class="score">{{seller.serviceScore}}</span>
-                        </div>
-                        <div class="score-wrapper">
-                            <span class="title">商品评分</span>
-                            <star :size='36' :score='seller.foodScore'></star>
-                            <span class="score">{{seller.foodScore}}</span>
-                        </div>
-                        <div class="delivery-wrapper">
-                            <span class="title">送达时间</span>
-                            <span class="delivery">{{seller.deliveryTime}}分钟</span>
-                        </div>
-                    </div>
-                </div>
-                <split></split>
-                <rating-select :ratings='ratings' @ratingTypeSelect='changeType' @contentToggle='toggleContent'></rating-select>
-                <div class="rating-wrapper">
-                    <ul>
-                        <li v-for="(rating,index) in ratings" :key="index" class="rating-item">
-                            <div class="avatar">
-                                <img :src="rating.avatar" alt="">
-                            </div>
-                            <div class="content">
-                                <h1 class="name">{{rating.username}}</h1>
-                                <div class="star-wrapper">
-                                    <star :size='24' :score='rating.score'></star>
-                                    <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
-                                </div>
-                                <p class="text">{{rating.text}}</p>
-                                <div class="recommend" v-show="rating.recommend && rating.recommend.length">
-                                    <span class="icon-thumb_up"></span>
-                                    <span v-for="(item,index) in rating.recommend" :key="index">{{item}}</span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+  <div>
+    <div class="ratings" ref="ratings">
+      <div class="ratings-content">
+        <div class="overview">
+          <div class="overview-left">
+            <h1 class="score">{{seller.score}}</h1>
+            <div class="title">综合评分</div>
+            <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+          </div>
+          <div class="overview-right">
+            <div class="score-wrapper">
+              <span class="title">服务态度</span>
+              <star :size='36' :score='seller.serviceScore'></star>
+              <span class="score">{{seller.serviceScore}}</span>
             </div>
+            <div class="score-wrapper">
+              <span class="title">商品评分</span>
+              <star :size='36' :score='seller.foodScore'></star>
+              <span class="score">{{seller.foodScore}}</span>
+            </div>
+            <div class="delivery-wrapper">
+              <span class="title">送达时间</span>
+              <span class="delivery">{{seller.deliveryTime}}分钟</span>
+            </div>
+          </div>
         </div>
+        <split></split>
+        <rating-select :ratings='ratings' @ratingTypeSelect='changeType' @contentToggle='toggleContent'></rating-select>
+        <div class="rating-wrapper">
+          <ul>
+            <li v-for="(rating,index) in ratings" :key="index" class="rating-item" v-show="needShow(rating.rateType,rating.text)">
+              <div class="avatar">
+                <img :src="rating.avatar" alt="" width="28" height="28">
+              </div>
+              <div class="content">
+                <h1 class="name">{{rating.username}}</h1>
+                <div class="star-wrapper">
+                  <star :size='24' :score='rating.score'></star>
+                  <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+                </div>
+                <p class="text">{{rating.text}}</p>
+                <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                  <span class="icon-thumb_up"></span>
+                  <span v-for="(item,index) in rating.recommend" :key="index" class="item">{{item}}</span>
+                </div>
+                <div class="time">
+                  {{rating.rateTime | formaDate}}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import star from '../star/star'
+import BScorll from 'better-scroll'
 import split from '../../components/split/split'
 import ratingSelect from '../../components/ratingselect/ratingselect'
 import selectType from '../../components/ratingselect/selectType'
+import { formaDate } from '../common/js/date'
 const ERR_OK = 0
 export default {
   props: {
@@ -82,27 +87,47 @@ export default {
       res = res.data
       if (res.errno === ERR_OK) {
         this.ratings = res.data
+        this.$nextTick(() => {
+          this.scroll = new BScorll(this.$refs.ratings, {
+            click: true
+          })
+        })
       }
     })
   },
+  filters: {
+    formaDate(time) {
+      let date = new Date(time)
+      return formaDate(date, 'yyyy-M-dd hh:mm')
+    }
+  },
   methods: {
+    needShow(type, text) {
+      if (this.onlyContent && !text) return false
+      if (this.selectType === selectType.ALL) {
+        return true
+      } else {
+        return type === this.selectType
+      }
+    },
     changeType(selectType) {
       this.selectType = selectType
-      //   this.$nextTick(() => {
-      //     this.scroll.refresh()
-      //   })
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
     },
     toggleContent(onlyContent) {
       this.onlyContent = onlyContent
-      //   this.$nextTick(() => {
-      //     this.scroll.refresh()
-      //   })
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../common/scss/mixin';
 .ratings {
   position: absolute;
   top: 174px;
@@ -179,6 +204,85 @@ export default {
           margin-left: 12px;
           line-height: 18px;
           font-size: 12px;
+          color: rgb(147, 153, 159);
+        }
+      }
+    }
+  }
+  .rating-wrapper {
+    padding: 0 18px;
+    .rating-item {
+      display: flex;
+      padding: 18px 0;
+      @include border-1px(rgba(7,17,27,0.1));
+      .avatar {
+        flex-basis: 28px;
+        width: 28px;
+        margin-right: 12px;
+        img {
+          width: 28px;
+          height: 28px;
+          border-radius: 50%;
+        }
+      }
+      .content {
+        flex: 1;
+        position: relative;
+        .name {
+          margin-bottom: 4px;
+          line-height: 12px;
+          font-size: 10px;
+          color: rgb(7, 17, 27);
+        }
+        .star-wrapper {
+          margin-bottom: 6px;
+          font-size: 0;
+          .star {
+            display: inline-block;
+            margin-right: 6px;
+            vertical-align: top;
+          }
+          .delivery {
+            display: inline-block;
+            vertical-align: top;
+            line-height: 12px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+        }
+        .text {
+          line-height: 18px;
+          color: rgb(7, 17, 27);
+          font-size: 12px;
+          margin-bottom: 6px;
+        }
+        .recommend {
+          line-height: 16px;
+          font-size: 0;
+          .icon-thumb_up,
+          .item {
+            display: inline-block;
+            margin-right: 8px;
+            margin-bottom: 4px;
+            font-size: 9px;
+          }
+          .icon-thumb_up {
+            color: rgb(0, 160, 220);
+          }
+          .item {
+            padding: 0 6px;
+            border: 1px solid rgba(7, 17, 27, 0.1);
+            border-radius: 1px;
+            color: rgb(147, 153, 159);
+            background-color: rgb(255, 255, 255);
+          }
+        }
+        .time {
+          position: absolute;
+          top: 0;
+          right: 0;
+          line-height: 12px;
+          font-size: 10px;
           color: rgb(147, 153, 159);
         }
       }
